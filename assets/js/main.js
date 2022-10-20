2189603cdfc472a0bd4c1f9898f6809b41c8e01d
MaTtiaa01 <mattia2001@live.it>
2022-10-20 13:46:29 +0200

    log in console of member

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 /*Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
 MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+    -use FOR in the ourTeam array to select each member
+    -log in console every property of each member 
+
+
 MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
 BONUS 1: Trasformare la stringa foto in una immagine effettiva
 BONUS 2: Organizzare i singoli membri in card/schede
@@ -38,3 +42,12 @@ const ourTeam = [
         image: "angela-caroll-chief-editor.jpg"
     }
 ]
+
+
+for (let i = 0; i < ourTeam.length; i++) {
+    const member = ourTeam[i];
+    console.log(member);
+    console.log(member.name);
+    console.log(member.role);
+    console.log(member.image);
+}
